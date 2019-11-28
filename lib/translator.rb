6280fdfd975b1b201420de7a4f8d83d7949fe622
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-28 06:10:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,19 +4,30 @@ require 'yaml'
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   # p emoticons 
-  emoticon_hash = { "get_meaning" => {}, "get_emoticon" => {} }
+  # ex: emoticons = { "angel" => ["O:)", "☜(⌒▽⌒)☞"],
+  # key "angel" is meaning 
+  # value is array of emoticons, first emoticon is English, second is Japanese 
   
-  # _____logic is still a bit fuzzy for the code below_____
+  emoticon_hash = { "get_meaning" => {}, "get_emoticon" => {} }
   
   emoticons.each do |meaning, emoticons| 
     emoticon_hash["get_meaning"][emoticons[1]] = meaning
+    # gives English meaning for Japanese emoticon
+    # "get_meaning" => {"☜(⌒▽⌒)☞"=>"angel",
     emoticon_hash["get_emoticon"][emoticons[0]] = emoticons[1]
+    # gives English emoticon for Japanese emoticon
+    # "get_emoticon" => {"O:)"=>"☜(⌒▽⌒)☞",
   end 
-  emoticon_hash
+   p emoticon_hash
+   
+   emoticon_hash
 end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+  # result = load_library(file_path)['get_emoticon'][emoticon]
+  # result ? result : "Sorry, that emoticon was not found"
+  
   emoticon_hash = load_library(file_path)
   if emoticon_hash["get_emoticon"].include?(emoticon)
     emoticon_hash["get_emoticon"][emoticon]
